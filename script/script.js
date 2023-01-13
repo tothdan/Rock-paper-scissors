820702009621e738d8a54ebd2d2314428f2069db
deevinex <72320807+deevinex@users.noreply.github.com>
2023-01-13 09:49:34 +0100

    updated JS file to ask for a selection every round

--- script/script.js
+++ script/script.js
@@ -1,36 +1,69 @@
-//function for the computer to choose from rock paper or scissors
-const computerSelection = getComputerChoice();
-const playerSelection = "Rock";
+let playerScore = 0;
+let computerScore = 0;
+const CHOICES = ["rock", "paper", "scissors"];
+
+
 function getComputerChoice(){
-    //generate random number from 1 to 3 for the choose of rock, paper or scissors
-    let randomNumber = Math.floor((Math.random()*3)+1);
-    console.log(randomNumber)
-    if(randomNumber === 1){
-        return "Rock";
-    }else if(randomNumber === 2){
-        return "Paper";
-    }else{
-        return"Scissors";
-    }
+    
+    let randomNum = Math.floor(Math.random()*CHOICES.length);
+    return CHOICES[randomNum];
     
 }
 
-
+function getPlayerChoice(){
+    let playerAnswer = prompt("Rock, paper or scissors?").toLowerCase();
+    return (playerAnswer === (CHOICES[0] || CHOICES[1] || CHOICES[2]) ? playerAnswer : "invalid answer, please write only rock paper or scissors")
+    
+    
+}
 
 function playRound(playerSelection, computerSelection){
-    if(playerSelection === computerSelection){
-        return "It's a tie!"
-    }else if(playerSelection === "Rock" && computerSelection === "Paper"){
-        return "You Lose! Paper beats Rock"
-    }else if(playerSelection === "Rock" && computerSelection === "Scissors"){
-        return "You Win! Rock beats Scissors!"
+    if(playerSelection === computerSelection)
+        console.log(`It's a tie! Your ${playerSelection} selection ties to computer's ${computerSelection} selection!`)
+    
+    if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ){
+        playerScore++;
+        console.log(`You Win! Your ${playerSelection} selection beats computer's ${computerSelection} selection!`)
+        
+    }
+    if (
+            (playerSelection === "rock" && computerSelection === "paper") ||
+            (playerSelection === "paper" && computerSelection === "scissors") ||
+            (playerSelection === "scissors" && computerSelection === "rock")
+    ){
+        computerScore++;
+        console.log(`You Lose! Your ${playerSelection} selection lose to computer's ${computerSelection} selection!`)
+        
     }
     
+    
 }
+
 
 
 function game(){
-    for(let i=0; i<5; i++){
-        
+    for(let i = 0; i < 5; i++){
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log(`Score: ${playerScore} - ${computerScore}`)
     }
+    gameEnd();
 }
+
+function gameEnd(){
+    
+    console.log(`Final score: ${playerScore} - ${computerScore}`)
+    
+
+}
+
+game()
+
+
+
+
